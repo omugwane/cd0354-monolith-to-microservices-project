ca41316d658a396cc85cb20f20215006bbaac741
Orly Mugwaneza <omugwaneza@qtsoftwareltd.com>
2022-10-10 18:03:34 +0200

    updated reverseproxy endpoint

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3a176cf72f1e4517a6f64eac6bb3a59-2095393590.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
